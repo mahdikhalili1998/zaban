c6f6ea65382932d9f45892c488c6fb8901089193
aidatabeii <aidatabeii3@gmail.com>
2024-09-04 09:50:25 -0700

    add new style

--- zaban/src/app/page.js
+++ zaban/src/app/page.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 function Pages() {
     return(
         <div>
-            <h1 class="w-96 h-40"> 
-                آموزش آنلاین تمامی تخصص ها 
-                <br></br>
-                کلاس بگیر,هزینه اش با میتیلو
+            <h1 class="text-zinc-500">
+                اگه بتونی از 18 مرداد تا 18 اسفند 70 جلسه کلاس زبان برگزار کنی
+                 هزینه آزمون ایلتس یا تافلت رو میتیلو پرداخت میکنه! 
             </h1>
+            <button class="text-white w-52 size-4 h-8 px-3 rounded-lg bg-purple-800">همین الان امتحان کن!</button>
             
 
         </div>
